edb121e06d48b10f5039e4e62e37bf6ca90d6e33
moonbadger13 <markmooney13@hotmail.com>
2024-05-19 17:23:17 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Basic JavaScript to add a simple interactive feature.
-document.addEventListener('DOMContentLoaded', function() {
-    // Example: Console log the sections when clicked
-    document.querySelectorAll('section').forEach(section => {
-        section.addEventListener('click', function() {
-            console.log('Section clicked:', this.id);
-        });
-    });
-});
